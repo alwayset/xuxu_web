18e7ae8a0aa0ac8f809e665c44daee8e00111a96
zmc <358939224@qq.com>
2014-02-25 20:21:22 +0800

    阿双方都是

---
+++ public/js/gallery.js
@@ -0,0 +1,52 @@
+$(function() {
+  AV.$ = jQuery;
+
+  // Initialize AV with your AV application javascript keys
+  AV.initialize("wzxcxb6x360ze0tcp0jrewnkz42ardd3wwnknfpgfl8k4huc",
+                   "3reqogjfk7eyv86r2khbmzmakw9rmcfcj0la0pgfk270mmm7");
+
+  // Todo Model
+  // ----------
+
+  // Our basic Todo model has `content`, `order`, and `done` attributes.
+  var PaWallObject = AV.Object.extend("PaWallObject");
+
+  // Todo Collection
+  // ---------------
+
+  var PaWallObjects = AV.Collection.extend({
+
+    // Reference to this collection's model.
+    model: PaWallObject,
+
+  });
+
+  //var strPut = "";
+  
+
+
+  var collection = new PaWallObjects();
+    collection.comparator = function(object) {
+    return object.get("createdAt");
+  };
+  collection.comparator = function(object) {
+    return object.get("createdAt");
+  };
+  collection.fetch({
+    success: function(collection) {
+      collection.each(function(object) {
+        //document.writeln(object.get("contentText")+"123\n");
+        var profilePhoto = object.get("thumbnail");
+        //strPut +="<img src="+profilePhoto.url()+"\">";
+        document.writeln("<img src=\""+profilePhoto.url()+"\"/>");
+        //document.writeln(profilePhoto.url());
+      });
+    },
+    error: function(collection, error) {
+      // The collection could not be retrieved.
+    }
+  });
+
+  //$("#divImg").html(strPut);//<div id="divImg"></div>
+
+});
