764014435a67b048ec7aded2e8a5aa9b8b85ba90
zmc <358939224@qq.com>
2014-02-25 18:47:38 +0800

    a try

---
+++ public/js/xuxu.js
@@ -0,0 +1,39 @@
+$(function() {
+
+  AV.$ = jQuery;
+
+  // Initialize AV with your AV application javascript keys
+  AV.initialize("wzxcxb6x360ze0tcp0jrewnkz42ardd3wwnknfpgfl8k4huc",
+                   "3reqogjfk7eyv86r2khbmzmakw9rmcfcj0la0pgfk270mmm7");
+
+  // Todo Model
+  // ----------
+
+  // Our basic Todo model has `content`, `order`, and `done` attributes.
+  var PaWallObject = AV.Object.extend("PaWallObject");
+
+  // Todo Collection
+  // ---------------
+
+  var PaWallObjects = AV.Collection.extend({
+
+    // Reference to this collection's model.
+    model: PaWallObject,
+
+    });
+  var collection = new PaWallObjects();
+  collection.fetch({
+    success: function(collection) {
+      collection.each(function(object) {
+        console.warn(object);
+      });
+    },
+    error: function(collection, error) {
+      // The collection could not be retrieved.
+    }
+  });
+  collection.comparator = function(object) {
+    return object.get("createdAt");
+  };
+
+});
