9d38787fc464f0d4431ad01446ee3b2bde6297d5
zmc1213 <358939224@qq.com>
2014-02-26 22:26:53 +0800

    a try

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -5,48 +5,6 @@ $(function() {
   AV.initialize("wzxcxb6x360ze0tcp0jrewnkz42ardd3wwnknfpgfl8k4huc",
                    "3reqogjfk7eyv86r2khbmzmakw9rmcfcj0la0pgfk270mmm7");
 
-  // Todo Model
-  // ----------
 
-  // Our basic Todo model has `content`, `order`, and `done` attributes.
-  var Picture = AV.Object.extend("Picture");
-
-  // Todo Collection
-  // ---------------
-
-  var Pictures = AV.Collection.extend({
-
-    // Reference to this collection's model.
-    model: Picture,
-
-  });
-
-  //var strPut = "";
-  
-
-
-  var collection = new Pictures();
-
-  collection.fetch({
-    success: function(collection) {
-      collection.each(function(object) {
-        //document.writeln(object.get("contentText")+"123\n");
-        alert(object.get("objectId"))
-        var profilePhoto = object.get("image");
-        //strPut +="<img src="+profilePhoto.url()+"\">";
-        alert(0);
-        document.writeln("<img src=\""+profilePhoto.url()+"\"/>");
-        alert(1);
-        alert(profilePhoto.url());
-        document.writeln(profilePhoto.url());
-      });
-    },
-    error: function(collection, error) {
-      // The collection could not be retrieved.
-    }
-  });
-  collection.comparator = function(object) {
-    return object.get("createdAt");
-  };
 
 });
