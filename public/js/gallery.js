d7f010e61b6ef984b97c35fbc272dc7d413aef84
zmc1213 <358939224@qq.com>
2014-02-26 23:16:44 +0800

    xxx

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -5,13 +5,5 @@ $(function() {
   AV.initialize("wzxcxb6x360ze0tcp0jrewnkz42ardd3wwnknfpgfl8k4huc",
                    "3reqogjfk7eyv86r2khbmzmakw9rmcfcj0la0pgfk270mmm7");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> d49ffe9545f323842d0696d9cdcd624019ab9145
-=======
-
->>>>>>> d49ffe9545f323842d0696d9cdcd624019ab9145
 
 });
