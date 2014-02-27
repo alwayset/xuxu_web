7f2d160cf727c5dd4945b004c1eba87a0678a5e5
zmc <358939224@qq.com>
2014-02-27 17:54:14 +0800

    456ytrrtbgrbtsf

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -5,5 +5,38 @@ $(function() {
   AV.initialize("wzxcxb6x360ze0tcp0jrewnkz42ardd3wwnknfpgfl8k4huc",
                    "3reqogjfk7eyv86r2khbmzmakw9rmcfcj0la0pgfk270mmm7");
 
+	var Picture = AV.Object.extend("Picture");
 
+	  // Todo Collection
+	  // ---------------
+
+	  var Pictures = AV.Collection.extend({
+
+	    // Reference to this collection's model.
+	    model: Picture,
+
+	  });
+
+	  //var strPut = "";
+	  
+
+
+	  var collection = new Pictures();
+	  collection.fetch({
+	    success: function(collection) {
+	      collection.each(function(object) {
+	        //document.writeln(object.get("contentText")+"123\n");
+	        var profilePhoto = object.get("image");
+	        //strPut +="<img src="+profilePhoto.url()+"\">";
+	        document.writeln("<img src=\""+profilePhoto.url()+"\"/>");
+	        //document.writeln(profilePhoto.url());
+	      });
+	    },
+	    error: function(collection, error) {
+	      // The collection could not be retrieved.
+	    }
+	  });
+	  collection.comparator = function(object) {
+	    return object.get("createdAt");
+	  };
 });
